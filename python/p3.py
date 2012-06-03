98367f40cdba5c475e2b4e954db87add2f7bd5fe
Sam Kimbrel <kimbrel@me.com>
2012-06-02 23:29:19 -0700

    Python: problem 3

---
+++ python/p3.py
@@ -0,0 +1,13 @@
+from math import sqrt
+
+NUMBER = 600851475143
+
+
+def is_prime(num):
+    for i in xrange(2, int(sqrt(num))):
+        if num % i == 0:
+            return False
+
+    return True
+
+print max([i for i in xrange(2, int(sqrt(NUMBER))) if NUMBER % i == 0 and is_prime(i)])
