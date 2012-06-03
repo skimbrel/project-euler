2da157fe9619a375748d6351b79f02ea3e98b7e0
Sam Kimbrel <kimbrel@me.com>
2012-06-03 11:08:29 -0700

    fix primality test

--- python/p3.py
+++ python/p3.py
@@ -1,10 +1,11 @@
+from math import floor
 from math import sqrt
 
 NUMBER = 600851475143
 
 
 def is_prime(num):
-    for i in xrange(2, int(sqrt(num))):
+    for i in xrange(2, int(floor(sqrt(num))) + 1):
         if num % i == 0:
             return False
 
