1a1b6a93149e97c29cec9373c9d1a56a12d8209a
Sam Kimbrel <kimbrel@me.com>
2012-06-02 23:20:35 -0700

    Python: problem 2

---
+++ python/p2.py
@@ -0,0 +1,9 @@
+def fibonacci(n):
+  a, b = 0, 1
+  while True:
+    if a > n:
+      raise StopIteration()
+    yield a
+    a, b = b, a + b
+
+print sum([fib for fib in fibonacci(4000000) if fib % 2 == 0])
